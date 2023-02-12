9d9478ce3b82e0cc0d3c6cc265e49f7a25d5b0f8
unknown <max.timofte@yahoo.com>
2023-02-12 19:41:37 +0200

    initial Working grpc server client with sent data in response

---
+++ gRPCclient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+namespace gRPCclient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // The port number must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:7220");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            var input=Console.ReadLine();
+            var reply2 = await client.SayHelloAsync(
+                              new HelloRequest { Name = input });
+            Console.WriteLine("Greeting: " + reply2.Message);
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
